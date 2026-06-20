9ecf2acaa87ea43a8791cbfac6dbcf451a63791f
felixdiamondvibes-collab <felixdiamondvibes@gmail.com>
2026-06-20 19:30:02 +0100

    Add Firebase initialization and configuration

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCikqHdja5ng3Gl5FIpC1J0HWV3poCNlcA",
+  authDomain: "trustai-web.firebaseapp.com",
+  projectId: "trustai-web",
+  storageBucket: "trustai-web.firebasestorage.app",
+  messagingSenderId: "89401090133",
+  appId: "1:89401090133:web:5418ac2b02bcf484f8f4dc",
+  measurementId: "G-QQD105CY2Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
